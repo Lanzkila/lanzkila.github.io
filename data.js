23c25e73faa7cd5a696bb310775e1198d2e2e01f
Lanz Magick <93784560+Lanzkila@users.noreply.github.com>
2026-07-31 08:21:42 +0800

    Change L4d2 Tools from private to public

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const blogData = [
     { name: "Studio Converter", url: "[https://github.com/Lanzkila/StudioConverter](https://github.com/Lanzkila/StudioConverter)", cat: "cat4", desc: "Tempat Converter Manga yang Mendukung Format Seperti CBZ,ZIP dan lain-lain.", isPrivate: false },
-    { name: "L4d2 Tools", url: "[https://github.com/Lanzkila/Tools-L4D2](https://github.com/Lanzkila/Tools-L4D2)", cat: "cat4", desc: "Tools Game Left 4 Dead 2.", isPrivate: true },
+    { name: "L4d2 Tools", url: "[https://github.com/Lanzkila/Tools-L4D2](https://github.com/Lanzkila/Tools-L4D2)", cat: "cat4", desc: "Tools Game Left 4 Dead 2.", isPrivate: false },
     { name: "@", url: "#", cat: "cat2", desc: "@", isPrivate: false }
     // Sambung tambah lagi di sini mengikut format ini
 ];
