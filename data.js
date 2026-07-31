3ccc8efc9545ed5bd2726734a8a111c971abe0a3
Lanz Magick <93784560+Lanzkila@users.noreply.github.com>
2026-07-31 08:58:37 +0800

    Fix formatting of blogData URLs in data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const blogData = [
-    { name: "Studio Converter", url: "[https://github.com/Lanzkila/StudioConverter](https://github.com/Lanzkila/StudioConverter)", cat: "cat4", desc: "Tempat Converter Manga yang Mendukung Format Seperti CBZ,ZIP dan lain-lain.", isPrivate: false },
-    { name: "L4d2 Tools", url: "[https://github.com/Lanzkila/Tools-L4D2](https://github.com/Lanzkila/Tools-L4D2)", cat: "cat4", desc: "Tools Game Left 4 Dead 2.", isPrivate: false },
-    { name: "Manga Reader", url: "[https://github.com/Lanzkila/Manga-Reader](https://github.com/Lanzkila/Manga-Reader)", cat: "cat4", desc: "Pembaca manga berasaskan web yang privasi-sentrik.", isPrivate: false },
-    { name: "Kirin PDF Reader", url: "[https://github.com/Lanzkila/Kirin-Dynamic-PDF](https://github.com/Lanzkila/Kirin-Dynamic-PDF)", cat: "cat4", desc: "Kirin PDF Reader adalah viewer PDF berasaskan web yang ringan dan pantas, direka khas untuk peminat manga dan dokumen panjang.", isPrivate: false }
+    { name: "Studio Converter", url: "https://github.com/Lanzkila/StudioConverter", cat: "cat4", desc: "Tempat Converter Manga yang Mendukung Format Seperti CBZ,ZIP dan lain-lain.", isPrivate: false },
+    { name: "L4d2 Tools", url: "https://github.com/Lanzkila/Tools-L4D2", cat: "cat4", desc: "Tools Game Left 4 Dead 2.", isPrivate: false },
+    { name: "Manga Reader", url: "https://github.com/Lanzkila/Manga-Reader", cat: "cat4", desc: "Pembaca manga berasaskan web yang privasi-sentrik.", isPrivate: false },
+    { name: "Kirin PDF Reader", url: "https://github.com/Lanzkila/Kirin-Dynamic-PDF", cat: "cat4", desc: "Kirin PDF Reader adalah viewer PDF berasaskan web yang ringan dan pantas, direka khas untuk peminat manga dan dokumen panjang.", isPrivate: false }
 ];
