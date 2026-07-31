2a99cb270149e0c65a316936d89268ff123ae79a
Lanz Magick <93784560+Lanzkila@users.noreply.github.com>
2026-07-31 08:10:37 +0800

    Add Studio Converter entry to blogData

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const blogData = [
-    { name: "@", url: "#", cat: "cat1", desc: "@", isPrivate: false },
+    { name: "Studio Converter", url: "https://github.com/Lanzkila/StudioConverter", cat: "cat1", desc: "Tempat Converter Manga yang Mendukung Format Seperti CBZ,ZIP dan lain-lain.", isPrivate: false },
     { name: "@", url: "#", cat: "cat1", desc: "@", isPrivate: true },
     { name: "@", url: "#", cat: "cat2", desc: "@", isPrivate: false }
     // Sambung tambah lagi di sini mengikut format ini
