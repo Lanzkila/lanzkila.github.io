9965cac7f584a9962dadc8004fae35715c490e20
Lanz Magick <93784560+Lanzkila@users.noreply.github.com>
2026-08-13 00:05:39 +0800

    Change 'isNew' status of Manga Reader to false

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const blogData = [
     { name: "Studio Converter", url: "https://github.com/Lanzkila/StudioConverter", cat: "cat4", desc: "Tempat Converter Manga yang Mendukung Format Seperti CBZ,ZIP dan lain-lain.", isPrivate: false, isNew: false },
     { name: "L4d2 Tools", url: "https://github.com/Lanzkila/Tools-L4D2", cat: "cat4", desc: "Tools Game Left 4 Dead 2.", isPrivate: false, isNew: false },
-    { name: "Manga Reader", url: "https://github.com/Lanzkila/Manga-Reader", cat: "cat4", desc: "Pembaca manga berasaskan web yang privasi-sentrik.", isPrivate: false, isNew: true },
+    { name: "Manga Reader", url: "https://github.com/Lanzkila/Manga-Reader", cat: "cat4", desc: "Pembaca manga berasaskan web yang privasi-sentrik.", isPrivate: false, isNew: false },
     { name: "Kirin PDF Reader", url: "https://github.com/Lanzkila/Kirin-Dynamic-PDF", cat: "cat4", desc: "Kirin PDF Reader adalah viewer PDF berasaskan web yang ringan dan pantas, direka khas untuk peminat manga dan dokumen panjang.", isPrivate: false, isNew: false }
 ];
